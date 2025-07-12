3e2b17799e2ad1cfc850c3cf41272e413d92e7b1
Ininsico <ininsico@gmail.com>
2025-07-12 13:46:33 +0500

    edit2

--- Algorithms/CirculatLinkedList/CircularLinkedList.java
+++ Algorithms/CirculatLinkedList/CircularLinkedList.java
@@ -1,7 +1,5 @@
-package CirculatLinkedList;
-
 public class CircularLinkedList {
-    class Node {
+    static class Node {
         int data;
         Node next;
 
@@ -11,73 +9,192 @@ public class CircularLinkedList {
         }
     }
 
-    class List {
-        Node head;
-        Node tail;
+    static class CircularLinkedListImplementation {
+        Node head = null;
+        Node tail = null;
 
-        List() {
-            Node head = null;
-            Node tail = null;
-        }
-
-        public void insertatbeganning(int data) {
-            Node newnode = new Node(data);
+        public void addAtBeginning(int data) {
+            Node newNode = new Node(data);
             if (head == null) {
-                head = newnode;
-                tail = newnode;
+                head = newNode;
+                tail = head;
                 tail.next = head;
             } else {
-                newnode.next = head;
-                head = newnode;
-                tail.next = head;
+                newNode.next = head;
+                tail.next = newNode;
+                head = newNode;
             }
         }
 
-        public void insertatend(int data) {
-            Node newnode = new Node(data);
+        public void addAtEnd(int data) {
+            Node newNode = new Node(data);
             if (head == null) {
-                head = newnode;
-                tail = newnode;
+                head = newNode;
+                tail = head;
                 tail.next = head;
             } else {
-                newnode.next = tail.next;
-                tail.next = newnode;
-                tail = newnode;
+                tail.next = newNode;
+                newNode.next = head;
+                tail = newNode;
             }
         }
 
-        public void addafteranode(int data, int key) {
-            Node newnode = new Node(data);
+        public void addAfterNode(int data, int key) {
             if (head == null) {
-                System.out.println("The list is empty");
-            } else {
-                Node current = head;
-                do {
-                    if (current.data == key) {
-                        newnode.next = current.next;
-                        current.next = newnode;
+                System.out.println("List is empty. Cannot add after.");
+                return;
+            }
+
+            Node current = head;
+            do {
+                if (current.data == key) {
+                    Node newNode = new Node(data);
+                    newNode.next = current.next;
+                    current.next = newNode;
+                    if (current == tail) {
+                        tail = newNode;
+                    }
+                    return;
+                }
+                current = current.next;
+            } while (current != head);
+
+            System.out.println("Node with value " + key + " not found.");
+        }
+
+        public void addBeforeNode(int data, int key) {
+            if (head == null) {
+                System.out.println("List is empty. Cannot add before.");
+                return;
+            }
+
+            Node prev = tail;
+            Node current = head;
+            do {
+                if (current.data == key) {
+                    Node newNode = new Node(data);
+                    newNode.next = current;
+                    prev.next = newNode;
+                    if (current == head) {
+                        head = newNode;
+                    }
+                    return;
+                }
+                prev = current;
+                current = current.next;
+            } while (current != head);
+
+            System.out.println("Node with value " + key + " not found.");
+        }
+
+        public void deleteNode(int key) {
+            if (head == null) {
+                System.out.println("List is empty. Nothing to delete.");
+                return;
+            }
+
+            Node prev = tail;
+            Node current = head;
+            boolean found = false;
+
+            do {
+                if (current.data == key) {
+                    if (current == head && current == tail) {
+                        head = tail = null;
+                    } else {
+                        prev.next = current.next;
+                        if (current == head) {
+                            head = current.next;
+                        }
                         if (current == tail) {
-                            tail = newnode;
+                            tail = prev;
                         }
                     }
-                } while (current.next != head && current.data != key);
-                if (current.data != key) {
-                    System.out.println("The Node you want to add the element next to is not found");
+                    found = true;
+                    break;
                 }
+                prev = current;
+                current = current.next;
+            } while (current != head);
+
+            if (!found) {
+                System.out.println("Node with value " + key + " not found.");
             }
         }
 
-        public void addbeforenode(int data, int key) {
-            Node newnode = new Node(data);
+        public void display() {
             if (head == null) {
-                System.out.println("The list is empty");
-            } else if (head.data == key) {
-                newnode.next = head;
-                Node current = head;
-                while (current.next != head) {
-
-                }
+                System.out.println("List is empty.");
+                return;
             }
+
+            Node current = head;
+            System.out.print("Circular Linked List: ");
+            do {
+                System.out.print(current.data);
+                if (current.next != head) {
+                    System.out.print(" -> ");
+                }
+                current = current.next;
+            } while (current != head);
+            System.out.println();
         }
+    }
+
+    public static void main(String[] args) {
+        CircularLinkedListImplementation cll = new CircularLinkedListImplementation();
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int choice;
+        
+        do {
+            System.out.println("\n--- Circular Linked List Operations ---");
+            System.out.println("1. Add at beginning");
+            System.out.println("2. Add at end");
+            System.out.println("3. Add after a node");
+            System.out.println("4. Add before a node");
+            System.out.println("5. Delete a node");
+            System.out.println("6. Display list");
+            System.out.println("7. Exit");
+            System.out.print("Enter your choice: ");
+            
+            choice = sc.nextInt();
+            
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter data to add at beginning: ");
+                    cll.addAtBeginning(sc.nextInt());
+                    break;
+                case 2:
+                    System.out.print("Enter data to add at end: ");
+                    cll.addAtEnd(sc.nextInt());
+                    break;
+                case 3:
+                    System.out.print("Enter data to add: ");
+                    int dataAfter = sc.nextInt();
+                    System.out.print("Enter key value after which to add: ");
+                    cll.addAfterNode(dataAfter, sc.nextInt());
+                    break;
+                case 4:
+                    System.out.print("Enter data to add: ");
+                    int dataBefore = sc.nextInt();
+                    System.out.print("Enter key value before which to add: ");
+                    cll.addBeforeNode(dataBefore, sc.nextInt());
+                    break;
+                case 5:
+                    System.out.print("Enter key value to delete: ");
+                    cll.deleteNode(sc.nextInt());
+                    break;
+                case 6:
+                    cll.display();
+                    break;
+                case 7:
+                    System.out.println("Exiting program...");
+                    break;
+                default:
+                    System.out.println("Invalid choice! Please try again.");
+            }
+        } while (choice != 7);
+        
+        sc.close();
     }
 }
